cbd3f63799b86275f0985d5f603656245a7d9718
isXander <xandersmith2008@gmail.com>
2022-01-12 16:14:52 +0000

    save oxygen levels to entity nbt improve oxygen effects

--- src/main/java/dev/isxander/moonmc/mixins/oxygen/MixinLivingEntity.java
+++ src/main/java/dev/isxander/moonmc/mixins/oxygen/MixinLivingEntity.java
@@ -1,13 +1,9 @@
 package dev.isxander.moonmc.mixins.oxygen;
 
-import dev.isxander.moonmc.oxygen.LivingEntityExtKt;
+import dev.isxander.moonmc.oxygen.EntityOxygenUtils;
 import dev.isxander.moonmc.oxygen.LivingEntityOxygenAccess;
-import dev.isxander.moonmc.registry.MoonRegistry;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+import net.minecraft.nbt.NbtCompound;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinLivingEntity implements LivingEntityOxygenAccess {
     @Shadow public abstract boolean isAlive();
 
-    private int oxygen = LivingEntityExtKt.MAX_OXYGEN;
+    private int oxygen = EntityOxygenUtils.MAX_OXYGEN;
 
     @Override public int getOxygen() { return oxygen; }
     @Override public void setOxygen(int oxygen) { this.oxygen = oxygen; }
@@ -26,7 +22,17 @@ public abstract class MixinLivingEntity implements LivingEntityOxygenAccess {
     @Inject(method = "baseTick", at = @At("RETURN"))
     public void updateOxygenTick(CallbackInfo ci) {
         if (this.isAlive()) {
-            LivingEntityExtKt.updateOxygen((LivingEntity) (Object) this);
+            EntityOxygenUtils.updateOxygen((LivingEntity) (Object) this);
         }
+    }
+
+    @Inject(method = "writeCustomDataToNbt", at = @At("RETURN"))
+    public void writeCustomNbt(NbtCompound nbt, CallbackInfo ci) {
+        EntityOxygenUtils.writeExtraNbt((LivingEntity) (Object) this, nbt);
+    }
+
+    @Inject(method = "readCustomDataFromNbt", at = @At("RETURN"))
+    public void readCustomNbt(NbtCompound nbt, CallbackInfo ci) {
+        EntityOxygenUtils.readExtraNbt((LivingEntity) (Object) this, nbt);
     }
 }
--- src/main/kotlin/dev/isxander/moonmc/oxygen/LivingEntityExt.kt
+++ src/main/kotlin/dev/isxander/moonmc/oxygen/LivingEntityExt.kt
@@ -1,3 +1,5 @@
+@file:JvmName("EntityOxygenUtils")
+
 package dev.isxander.moonmc.oxygen
 
 import net.minecraft.block.Blocks
@@ -7,8 +9,8 @@ import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.entity.mob.WaterCreatureEntity
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.nbt.NbtCompound
 import net.minecraft.util.math.BlockPos
-import kotlin.math.ceil
 
 const val MAX_OXYGEN = 12_000
 
@@ -32,9 +34,26 @@ fun LivingEntity.updateOxygen() {
         40
     }
 
-    if (oxygen <= 3000) {
-        val amplifier = 3 - (oxygen / 1000f).coerceIn(0f..3f)
-        this.addStatusEffect(StatusEffectInstance(StatusEffects.BLINDNESS, 100, ceil(amplifier).toInt(), true, false, false))
-        damage(OxygenDamageSource, amplifier)
+    if (oxygen % 20 == 0) {
+        val amplifier = when (oxygen) {
+            in 2000..2999 -> 0
+            in 1000..1999 -> 1
+            in 0..999 -> 2
+            else -> return
+        }
+        addStatusEffect(StatusEffectInstance(StatusEffects.BLINDNESS, 100, amplifier, false, false, false))
+        damage(OxygenDamageSource, amplifier.toFloat())
+
+        if (amplifier >= 1) {
+            addStatusEffect(StatusEffectInstance(StatusEffects.NAUSEA, 100, amplifier - 1, false, false, false))
+        }
     }
+}
+
+fun LivingEntity.writeExtraNbt(nbt: NbtCompound) {
+    nbt.putInt("oxygen", oxygen)
+}
+
+fun LivingEntity.readExtraNbt(nbt: NbtCompound) {
+    oxygen = nbt.getInt("oxygen")
 }
